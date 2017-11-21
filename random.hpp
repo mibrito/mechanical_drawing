e2d855b5eeeaa44ba2461ca304a16f2c3027b3c5
Michele Brito Pinheiro <mibrito@dcc.ufmg.br>
2017-11-21 11:09:25 -0200

    change random to work on mac

--- random.hpp
+++ random.hpp
@@ -12,23 +12,27 @@ private:
   static unsigned seed;
   static std::default_random_engine generator;
 public:
-  int width, height;
-  std::uniform_real_distribution<double> distRealUniform {0.0, 1.0};
-  std::uniform_int_distribution<int> distIntUniform {1,INT_MAX};
-  std::uniform_int_distribution<int> distGray {1,255};
+  std::uniform_real_distribution<double> distRealUniform;
+  std::uniform_int_distribution<int> distIntUniform;
+  std::uniform_int_distribution<int> distGray ;
   std::uniform_int_distribution<int> distX;
   std::uniform_int_distribution<int> distY;
   std::uniform_int_distribution<int> distThickness;
-  std::uniform_int_distribution<int> distThickness2{1,2};
-  std::uniform_int_distribution<int> distMutation{0,1};
-  std::uniform_int_distribution<int> distMutationChangeLeaf{0,6};
+  std::uniform_int_distribution<int> distThickness2;
+  std::uniform_int_distribution<int> distMutation;
+  std::uniform_int_distribution<int> distMutationChangeLeaf;
 
   R() {}
   R(int const& width, int const &height)
-    : width{width}, height{height},
+    : distRealUniform{std::uniform_real_distribution<double> {0.0, 1.0}},
+      distIntUniform{ std::uniform_int_distribution<int> {1,INT_MAX} },
+      distGray{ std::uniform_int_distribution<int> {1,255} },
       distX{ std::uniform_int_distribution<int> {1, width} },
       distY{ std::uniform_int_distribution<int> {1, height} },
-      distThickness{ std::uniform_int_distribution<int> {1, (int)std::min(width,height) } }
+      distThickness{ std::uniform_int_distribution<int> {1, (int)std::min(width,height) } },
+      distThickness2{ std::uniform_int_distribution<int> {1,2} },
+      distMutation{ std::uniform_int_distribution<int> {0, 4} },
+      distMutationChangeLeaf{ std::uniform_int_distribution<int> {0, 7} }
   {}
 
   double runif ();
